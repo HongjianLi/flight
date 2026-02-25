d6b407fc91dbb708a8f9e6c635725da424d63fd9
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-02-25 16:24:01 +0800

    Enable  export PUPPETEER_EXECUTABLE_PATH in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo "$(date +"%F %T.%N") Script started"
-#export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
-#export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
 echo "$(date +"%F %T.%N") node weather.js"
