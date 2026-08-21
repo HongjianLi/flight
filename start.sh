8600522aecbb32359e9409aa9bfdfc1263927cad
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-08-21 17:04:29 +0800

    Replace PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium by /usr/bin/google-chrome

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "$(date +"%F %T.%N") Script started"
-export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
 echo "$(date +"%F %T.%N") node weather.js"
